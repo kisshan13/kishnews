d471e2ad7e92aad58d1e6c986a5adc5e8c140e46
kisshan13 <kisshan07@outlook.com>
2023-01-18 18:55:07 +0530

    api done :)

--- src/api/api.js
+++ src/api/api.js
@@ -3,3 +3,26 @@ import ckey from "ckey";
 
 const kishnews = new NewsAPI(ckey.API_KEY)
 
+async function getHeadlines () {
+    let news = await kishnews.v2.topHeadlines({language: 'en', country: 'in'})
+
+    return news
+}
+
+/**
+ * 
+ * @param {String} category 
+ * 
+ * Valid Category: `business` , `entertainment`, `general`, `health`, `science`, `sports`, `technology`.
+ */
+async function getByCategory(category) {
+    let news = await kishnews.v2.topHeadlines({
+        category: category,
+        language: 'en',
+        country: 'in'
+    })
+
+    return news
+}
+
+export { getHeadlines, getByCategory }
